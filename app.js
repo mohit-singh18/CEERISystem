8a3022a5bc8b9e94f6bf85932757872521172332
mohit-singh18 <113810424+mohit-singh18@users.noreply.github.com>
2024-01-28 06:36:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,33 +7,25 @@ const sensorReadings = require("./routes/sensor-readings");
 const homePage = require("./routes/homePage");
 const path = require("path");
 const cors = require("cors");
-const webpush = require("web-push");
 const bodyParser = require("body-parser");
-const notificationRoute = require("./routes/notificationRoute");
 
 const app = express();
 //
 dotenv.config();
-const public_key =
-  "BHYvQoGPabiaYCItSzuDI8APJskp-KsbFOd2R_6UJBQWdgWYdLh39aN_x0lmJFh5bsOV1V9x0i1jNlaGZHdD0tM";
 //
 //INIT
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => {
-//     console.log("DB Connected");
-//   })
-//   .catch((e) => {
-//     console.log(`${e}`);
-//   });
-webpush.setVapidDetails(
-  "mailto:test@test.com",
-  public_key,
-  process.env.private_key
-);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((e) => {
+    console.log(`${e}`);
+  });
+
 //
 app.use(express.static(path.join(__dirname, "./frontend_files")));
 app.use(express.static(path.join(__dirname, "./frontend_files/files")));
@@ -41,7 +33,6 @@ app.use(express.static(path.join(__dirname, "./frontend_files/files")));
 app.use("/", homePage);
 app.use("/api/", authRoute);
 app.use("/", sensorReadings);
-app.use("/", notificationRoute);
 
 //
 app.listen(
